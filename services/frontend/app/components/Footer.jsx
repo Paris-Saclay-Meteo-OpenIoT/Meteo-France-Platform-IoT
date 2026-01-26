b72700411033e7f2374b38f78981713f648723be
ClementFenneteaux <clement.fenneteaux@gmail.com>
2026-01-26 16:49:19 +0100

    Update code année dernière

--- services/frontend/app/components/Footer.jsx
+++ services/frontend/app/components/Footer.jsx
@@ -2,7 +2,7 @@
 export default function Footer() {
     return (
         <footer className="bg-gray-800 text-white text-center py-3">
-          © TER 2025
+          © TER 2026
         </footer>
     );
 }
